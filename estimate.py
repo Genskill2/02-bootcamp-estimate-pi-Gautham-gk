63347648080c00264e10d1f996231f5dcb33307e
Gautham Krishna <gk22022001@gmail.com>
2021-05-31 13:26:42 +0530

    Modified estimate.py

--- estimate.py
+++ estimate.py
@@ -1,6 +1,6 @@
 import math
 import unittest
-
+import random
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
@@ -26,7 +26,33 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
+
+
+
+def wallis(n):
+    pival=1
+    for i in range(1,n+1):
+        pival*=(4*i*i)/(4*i*i-1)
+    pival*=2
+    print(pival)
+    return pival
+
+
+def monte_carlo(num):
+    total=0
+    inside=0
+    for j in range(num):
+        x=random.random()
+        y=random.random()
+        dist=(x**2+y**2)**0.5
+        if dist<1:
+            inside+=1
+        total+=1
+    print(inside,total)
+    pival=4*inside/total
+    print(pival)
+    return pival
+
     
 if __name__ == "__main__":
     unittest.main()
